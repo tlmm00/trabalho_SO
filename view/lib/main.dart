515df7bc2e73eaa88640fbf62d922b1d568a85ca
LadraoDeHavaiana <tluigimm@gmail.com>
2024-07-17 00:26:24 -0300

    inico conexão com backend pythonanywhere

--- view/lib/main.dart
+++ view/lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:input_quantity/input_quantity.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:http/http.dart' as http;
 import 'card.dart';
 
 void main() {
@@ -20,145 +23,192 @@ class _MyAppState extends State<MyApp> {
   // 2 => RR
   // 3 => SJF
   final List<bool> _selectedList = <bool>[true, false, false, false];
+  int selectedMethodId = 1;
+
   List<Widget> cardList = [];
   int quantum = 0;
   int overload = 0;
 
   final CarouselController _controller = CarouselController();
 
+  Map<String, dynamic> data = {};
+  Future<void> conn(int methodId) async {
+    String url = 'https://tlmm00.pythonanywhere.com/';
+    if (methodId == 0) {
+      // FIFO
+      url += 'FIFO';
+    } else if (methodId == 1) {
+      // EDF
+      url += 'EDF';
+    } else if (methodId == 2) {
+      // FF
+      url += 'RR';
+    } else {
+      // SJF
+      url += 'SJF';
+    }
+
+    print(url);
+
+    Map data = {};
+    for (int i in cardList.length) {}
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        data = json.decode(response.body);
+      });
+    } else {
+      print('Error getting data');
+    }
+    //return response;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 58, 112, 183)),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text("Trabalho SO"),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 58, 112, 183)),
+          useMaterial3: true,
         ),
-        body: Expanded(
-          flex: 1,
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              title: const Text("Trabalho SO"),
+            ),
+            body: Expanded(
+              flex: 1,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
+                    Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        ElevatedButton(
-                            onPressed: () => {setState(() => cardList.add(ProcessCard(processId: cardList.length)))},
-                            child: const Text("+")),
-                        const Divider(),
-                        ElevatedButton(
-                            onPressed: () => {setState(() => cardList.removeLast())},
-                            child: const Text("-")),
-                      
-                        Column(
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            Row(
+                            ElevatedButton(
+                                onPressed: () => {
+                                      setState(() => cardList.add(ProcessCard(
+                                          processId: cardList.length)))
+                                    },
+                                child: const Text("+")),
+                            const Divider(),
+                            ElevatedButton(
+                                onPressed: () =>
+                                    {setState(() => cardList.removeLast())},
+                                child: const Text("-")),
+                            Column(
                               children: [
-                                Text("Quantum: "),
-                                InputQty(
-                                  minVal: 0,
-                                  initVal: 0,
-                                  onQtyChanged: (value) => {quantum = value},
-                                )
-                              ]
+                                Row(children: [
+                                  Text("Quantum: "),
+                                  InputQty(
+                                    minVal: 0,
+                                    initVal: 0,
+                                    onQtyChanged: (value) => {
+                                      setState(() {
+                                        quantum = value;
+                                      })
+                                    },
+                                  )
+                                ]),
+                                Row(children: [
+                                  Text("Overload: "),
+                                  InputQty(
+                                    minVal: 0,
+                                    initVal: 0,
+                                    onQtyChanged: (value) => {
+                                      setState(() {
+                                        overload = value;
+                                      })
+                                    },
+                                  )
+                                ]),
+                              ],
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 500,
+                          child: CarouselSlider(
+                            options: CarouselOptions(),
+                            carouselController: _controller,
+                            items: cardList,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Flexible(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  _controller.previousPage();
+                                },
+                                child: const Text('<'),
+                              ),
                             ),
-                            Row(
-                              children: [
-                                Text("Overload: "),
-                                InputQty(
-                                  minVal: 0,
-                                  initVal: 0,
-                                  onQtyChanged: (value) => {overload = value},
-                                )
-                              ]
+                            Flexible(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  _controller.nextPage();
+                                },
+                                child: const Text('>'),
+                              ),
                             ),
                           ],
-                        )
+                        ),
                       ],
                     ),
-                    SizedBox(
-                      height: 500,
-                      child: CarouselSlider(
-                        options: CarouselOptions(),
-                        carouselController: _controller,
-                        items: cardList,
-                      ),
+                    Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ToggleButtons(
+                              isSelected: _selectedList,
+                              onPressed: (int index) {
+                                setState(() {
+                                  for (int i = 0;
+                                      i < _selectedList.length;
+                                      i++) {
+                                    _selectedList[i] = i == index;
+                                  }
+                                });
+                              },
+                              children: const [
+                                Text("FIFO"),
+                                Text("EDF"),
+                                Text("RR"),
+                                Text("SJF")
+                              ])
+                        ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        FloatingActionButton(
+                            onPressed: () => {
+                                  print("START"),
+                                  setState(() {
+                                    selectedMethodId = _selectedList
+                                        .indexWhere((value) => value == true);
+                                  }),
+                                  conn(selectedMethodId),
+                                },
+                            backgroundColor: Colors.green,
+                            child: Text("START")),
+                        FloatingActionButton(
+                            onPressed: () => {print("RESET")},
+                            backgroundColor: Colors.red,
+                            child: Text("RESET"))
+                      ],
                     ),
-                                  Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Flexible(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        _controller.previousPage();
-                      },
-                      child: const Text('<'),
-                    ),
-                  ),
-                  Flexible(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        _controller.nextPage();
-                      },
-                      child: const Text('>'),
-                    ),
-                  ),
-                ],
-              ),
-              ],
-            ), 
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ToggleButtons(
-                  isSelected: _selectedList,
-                    onPressed: (int index) {
-                    setState(() {
-                      for (int i = 0; i < _selectedList.length; i++){
-                        _selectedList[i] = i == index;
-                      }
-                    });
-                    },
-                  children: const [
-                    Text("FIFO"),
-                    Text("EDF"),
-                    Text("RR"),
-                    Text("SJF")
-                  ])
-              ]),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                FloatingActionButton(
-                  onPressed: () => {
-                    print("START")
-                  },
-                  backgroundColor: Colors.green,
-                  child: Text("START")),
-                FloatingActionButton(
-                  onPressed: () => {
-                    print("RESET")
-                  },
-                  backgroundColor: Colors.red,
-                  child: Text("RESET"))
-              ],
-            ),
-          ]),
-        ))
-      );
+                  ]),
+            )));
   }
 }
